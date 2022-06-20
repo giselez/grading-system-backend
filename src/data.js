f471bcef748d1796eaa53b590e13a42348a92a1b
plexity <plexthesavior@gmail.com>
2022-06-20 15:00:07 -0400

    data

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const data = {
+    student: [
+        {
+            id: 1,
+            name: 'Jeff Anderson'
+        },
+        {
+            id: 2,
+            name: 'Pat Smith'
+        },
+        {
+            id: 3,
+            name: 'Jane Johnston'
+        }
+    ],
+    teachers: [
+        {
+            id: 1,
+            name: 'Alan Mckenzie'
+        },
+        {
+            id: 2,
+            name: 'Robert Kidman'
+        },
+        {
+            id: 3,
+            name: 'Shawn McCarthy'
+        }
+    ],
+    classes:
+        [
+            {
+                teacherid: 1,
+                studentid: 1,
+                grade: '67'
+            },
+            {
+                teacherid: 2,
+                studentid: 3,
+                date: '25'
+            }
+        ]
+};
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express"; // importing the module
 const app = express(); // creating an Express app
-const { PORT = 3050 } = process.env;
+const { PORT = 3030 } = process.env;
 import bodyParser from "body-parser";
 import cors from "cors";
+import data from "../data";
+
 app.use(bodyParser.json()).use(cors());
 app.get("/", (request, response) => response.send("Hello World!"));
 // server will start listening for requests, the function is called immediately
 //once the server is ready. Console.logs show up in your terminal.
 app.listen(PORT, () =>
 console.log(`Hello World, I'm listening on port ${PORT}!`));
+
