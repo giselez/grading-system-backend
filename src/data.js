877f58ddb86017ce102128e6a95fbae8abcc0dbf
Tiancheng Wang <107882951+tiancheng710@users.noreply.github.com>
2022-06-21 09:52:48 -0400

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,43 +1,81 @@
-const data = { 
-    student: [
+const data = {
+    students: [
         {
             id: 1,
-            name: 'Jeff Anderson'
+            name: 'Jeff Anderson',
+            faculty: 'Engineering'
         },
         {
             id: 2,
-            name: 'Pat Smith'
+            name: 'Pat Smith',
+            faculty: 'Mathematics'
         },
         {
             id: 3,
-            name: 'Jane Johnston'
-        }
+            name: 'Jane Johnston',
+            faculty: 'Mathematics'
+        },
+        {
+            id: 4,
+            name: 'Linda Yu',
+            faculty: 'Science'
+        },
+        {
+            id: 5,
+            name: 'Alexander Moore',
+            faculty: 'Science'
+        },
     ],
     teachers: [
         {
             id: 1,
-            name: 'Alan Mckenzie'
+            name: 'Alan Mckenzie',
+            faculty: 'Engineering'
         },
         {
             id: 2,
-            name: 'Robert Kidman'
+            name: 'Robert Kidman',
+            faculty: 'Mathematics'
         },
         {
             id: 3,
-            name: 'Shawn McCarthy'
+            name: 'Shawn McCarthy',
+            faculty: 'Science'
         }
     ],
-    classes:
+    Marking:
         [
             {
+
                 teacherid: 1,
                 studentid: 1,
-                grade: '67'
+                grade: 83,
+                commentFromProf: 'You had an excellent performance, and you had done well on assignments and exams.'
             },
             {
                 teacherid: 2,
+                studentid: 2,
+                grade: 75,
+                commentFromProf: 'You had a good performance, and you should put more efforts towards assignments to improve your final grades.'
+            },
+            {
+
+                teacherid: 2,
                 studentid: 3,
-                date: '25'
+                grade: 95,
+                commentFromProf: 'You had an outstanding performance, and you did assignments, quizzes and exams perfectly. You should be pround of your work.'
+            },
+            {
+                teacherid: 3,
+                studentid: 4,
+                grade: 62,
+                commentFromProf: 'You had a moderate performance. You should be more active in class and submit assignments on time.'
+            },
+            {
+                teacherid: 3,
+                studentid: 4,
+                grade: 47,
+                commentFromProf: 'You had a poor performance, which means you failed and you should think about retaking this course. If you have any questions towards your final grades, please contact the Professor before next term starts.'
             }
         ]
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ const app = express(); // creating an Express app
 const { PORT = 3030 } = process.env;
 import bodyParser from "body-parser";
 import cors from "cors";
-import data from "../data";
+import data from "./data.js";
 
 app.use(bodyParser.json()).use(cors());
-app.get("/", (request, response) => response.send("Hello World!"));
-// server will start listening for requests, the function is called immediately
-//once the server is ready. Console.logs show up in your terminal.
-app.listen(PORT, () =>
-console.log(`Hello World, I'm listening on port ${PORT}!`));
 
+app.get("/", (request, response) => response.send("Student Grading System"));
+
+app.get("/api/teachers", (req, res) => {
+    res.json(data.teachers);
+});
+
+app.get("/api/students", (req, res) => {
+    res.json(data.students);
+});
+
+app.get("/api/marking", (req, res) => {
+    res.json(data.Marking);
+});
+
+app.get("/api/teachers/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const teacher = data.teachers.find( (ta) => ta.id === id );
+    if(!teacher){
+        res.status(404).json({error:"Teacher is not found, please try with another id!"});
+    }
+    return res.json(teacher);
+});
+
+app.get("/api/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const student = data.students.find( (st) => st.id === id );
+    if(!teacher){
+        res.status(404).json({error:"Student is not found, please try with another id!"});
+    }
+    return res.json(student);
+});
+
+app.listen(PORT, () =>
+console.log(`I'm listening http://localhost:${PORT}/`));
+
+export default app;
