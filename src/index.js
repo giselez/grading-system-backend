b41518a0c2e9f15a3e52ca73bdae67849da77ec0
esyed3426 <esyed3426@gmail.com>
2022-06-21 17:24:59 +0300

    added documentation, fixed and tested API endpoints

--- src/index.js
+++ src/index.js
@@ -7,20 +7,27 @@ import data from "./data.js";
 
 app.use(bodyParser.json()).use(cors());
 
+// All API endpoints are tested with Postman
+
+// A GET request to the base url endpoint will return the message "Student Grading System"
 app.get("/", (request, response) => response.send("Student Grading System"));
 
+// A GET request to the teachers endpoint will return a list of all teachers
 app.get("/api/teachers", (req, res) => {
     res.json(data.teachers);
 });
 
+// A GET request to the students endpoint will return a list of all students
 app.get("/api/students", (req, res) => {
     res.json(data.students);
 });
 
-app.get("/api/marking", (req, res) => {
-    res.json(data.Marking);
+// A GET request to the markings endpoint will return a list of all markings
+app.get("/api/markings", (req, res) => {
+    res.json(data.markings);
 });
 
+// A GET request to the teachers endpoint with id will return the teacher with that id or an error
 app.get("/api/teachers/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const teacher = data.teachers.find( (ta) => ta.id === id );
@@ -30,16 +37,16 @@ app.get("/api/teachers/:id", (req, res) => {
     return res.json(teacher);
 });
 
+// A GET request to the students endpoint with id will return the student with that id or an error
 app.get("/api/students/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const student = data.students.find( (st) => st.id === id );
-    if(!teacher){
+    if(!student){
         res.status(404).json({error:"Student is not found, please try with another id!"});
     }
     return res.json(student);
 });
 
-app.listen(PORT, () =>
-console.log(`I'm listening http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(`I'm listening http://localhost:${PORT}/`));
 
 export default app;
